9708ca69e9bb0d785de984f09be577a0f3f72e67
Hugo Nguyen <hugoq.nguyen@gmail.com>
2025-04-24 00:01:01 +0700

    Initialize source code structure

---
+++ src/main/java/org/crypto/trading/system/CryptoTradingSystemApplication.java
@@ -0,0 +1,12 @@
+package org.crypto.trading.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CryptoTradingSystemApplication.class, args);
+  }
+}
